f8d70b8d2a6b6d0cb36eeaeca5753e146f884a00
Adam Emrts <adam.emrts@gmail.com>
2025-11-15 14:03:50 +0100

    Trying first formula.

---
+++ Formula/rasterflow.rb
@@ -0,0 +1,25 @@
+class RasterFlow < Formula
+  desc "RasterFlow"
+  homepage "https://github.com/flatscrew/rasterflow"
+  url "https://github.com/flatscrew/rasterflow/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "c9d3ab6c87df1e75eb0acc44118f37ea45a1833b853aace26413fab22d06a35a"
+  license "GPL-3.0"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vala" => :build
+  depends_on "glib"
+  depends_on "gtk4"
+  depends_on "libadwaita"
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "#{bin}/rasterflow", "--version"
+  end
+end
