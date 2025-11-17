d78dab35edda807bcd20f7d935d77501bdf9fb56
activey <activey@doaplatform.org>
2025-11-17 14:20:43 +0100

    One last time...

--- Formula/rasterflow.rb
+++ Formula/rasterflow.rb
@@ -10,14 +10,14 @@ class Rasterflow < Formula
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
 
+  depends_on "flatscrew/osx/libgflow"
+  depends_on "flatscrew/osx/libgtkflow4"
+  
   depends_on "gegl"
   depends_on "glib"
   depends_on "gobject-introspection"
   depends_on "gtk4"
   depends_on "json-glib"
-  
-  depends_on "flatscrew/osx/libgflow"
-  depends_on "flatscrew/osx/libgtkflow4"
   
   depends_on "libadwaita"
   depends_on "libgee"
